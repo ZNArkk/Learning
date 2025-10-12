3b296e50bd5180ac664c790bbcabc32b9e48de4a
ZNArkk <znark@qq.com>
2025-10-13 01:10:01 +0800

    heapsort.cpp

--- 模板/heapsort.cpp
+++ 模板/heapsort.cpp
@@ -1,44 +1,43 @@
 //
-​#include #include <stdio.h><stdio.h> <stdio.h>#include<stdio.h>#include
-#include
-常量
-整数[
-void交换整数
-整数 t = a;
-a=b；
+#include <stdio.h>
+#include <iostream>
+const int MAX = 2e5 + 10;
+int n,a[MAX],tot;
+void swap(int &a,int &b) {
+    int t = a;
+    a = b;
     b = t;
 }
-无效下移值 voidvoid down(int i,int x) {down i，( x
+void down(int i,int x) {
     int s = i;
-    int l = 2 * i + 1, r = 2 * i + 2;int l = 2 * i + 1, r = 2 * i + 2;2 * i + 1, r = 2 * i + 
-    if (l <= x && a[s] < a[l]) s = l;ifs(l <= x && a[s] < a[l]]​​​l[l <= x && a) < a]​​ s = l;
-    if (r <= x && a[s] < a[r]) s = r;ifs(r <= x && a[s]x && a​s=r；x < a[
-如果（s！= 如果（） ​if    if(s != i) {if） 
-        swap(a[i],a[s]);swapa[i[s]​​,)i]​s[,a[​a(​​)]​​
-        down(s,x);downi]，x）；(,x​
-    }}}
+    int l = 2 * i + 1, r = 2 * i + 2;
+    if (l <= x && a[s] < a[l]) s = l;
+    if (r <= x && a[s] < a[r]) s = r;
+    if(s != i) {
+        swap(a[i],a[s]);
+        down(s,x);
+    }
 }
-​ buildheap（voidvoid buildheap(int x) {buildheap ​ int x）如果（​​​buildheapx x） {​x​ ​
-    for (int i = (x + 1) / 2 - 1; i >= 0; i--)for） / int0 i = (x + 1; i--）) / 2​ i = int - 
-i, x);
+void buildheap(int x) {
+    for (int i = (x + 1) / 2 - 1; i >= 0; i--)
+        down(i, x);
 }
-​ heapsort x） int(​​(​ ​） ​​int x)​​
-正在…tot    while (tot) {while）whilewhile​(​ ​
-        swap(a[tot],a[0]);])​​​​总计)​总计(a]​​​​​a]​tot​;)0​​​,a
-        down(0,--tot);down0，--总数）；0;
-    }}}
+void heapsort(int x) {
+    while (tot) {
+        swap(a[tot],a[0]);
+        down(0,--tot);
+    }
 }
-​在 main（） 整数（）intint main() {(
+int main() {
     std::cin >> n;
-    tot = n - 1;1;1;
-    for (int i = 0; i < n; i++)for​​ i = ​​( ​取整 i = int i = 
-        std::cin >> a[i];cin a[i]；​irr
-    buildheap(tot);buildheap总计​​;
-heapsort    heapsort(tot);totheapsort​void​void​heapsort(int heapsort(总计
-    for (int i = 0; i < n; i++)for​取整 
-        std::cout << a[i] << ' ';[i] << ' ';；[i] << ' ';];
+    tot = n - 1;
+    for (int i = 0; i < n; i++)
+        std::cin >> a[i];
+    buildheap(tot);
+    heapsort(tot);
+    for (int i = 0; i < n; i++)
+        std::cout << a[i] << ' ';
     return 0;  //Zk...
 }
 // Created by ZNArk on 2025/10/13.
-
 //
